55b94f35f0734825f34051a8674839ffb010f1f6
Edwin Gamboa <Edwin.Gamboa@incowia.com>
2016-07-12 13:46:23 +0200

    [BDE-255] Add support to change and display current Store url

---
+++ webpackages/bde/bde/elements/bde-store-settings/bde-store-settings.js
@@ -0,0 +1,86 @@
+/*global XMLHttpRequest*/
+Polymer({
+  is: 'bde-store-settings',
+
+  properties: {
+
+    opened: {
+      type: Boolean,
+      value: false
+    },
+
+    settings: {
+      type: Object,
+      notify: true
+    },
+
+    requestSuccess: {
+      type: Boolean
+    }
+
+  },
+
+  listeners: {
+    'iron-overlay-opened': 'handleOpened'
+  },
+
+  handleOpened: function () {
+    this.set('_intermediate', JSON.parse(JSON.stringify(this.settings)));
+  },
+
+  validateStoreSettings: function (e) {
+    if (this.$.storeForm.validate()) {
+      var changeBaseUrl;
+      var changeStoreName;
+      if (this.settings.store !== this.$.storeName.value) {
+        changeBaseUrl = true;
+      }
+      if (this.settings.baseUrl !== this.$.baseUrl.value) {
+        changeStoreName = true;
+      }
+      if (changeBaseUrl || changeStoreName) {
+        this.testStoreConnection(function () {
+          if (this.requestSuccess) {
+            this.changeStore(changeBaseUrl, changeStoreName);
+            this.$.formDialog.close();
+            this.showMessage('Store changed');
+          } else {
+            this.showMessage('Store url is not valid. The connection test was unsuccessful');
+          }
+        }.bind(this));
+      } else {
+        this.showMessage('The provided Store url is the same as the current one');
+      }
+    }
+  },
+
+  changeStore: function (changeBaseUrl, changeStoreName) {
+    if (changeBaseUrl) {
+      this.set('settings.' + this.$.storeName.name, this.$.storeName.value);
+    }
+    if (changeStoreName) {
+      this.set('settings.' + this.$.baseUrl.name, this.$.baseUrl.value);
+    }
+    document.querySelector('bde-app').resetBDE();
+  },
+
+  showMessage: function (message) {
+    this.$.toast.text = message;
+    this.$.toast.open();
+  },
+
+  testStoreConnection: function (callback) {
+    var xhr = new XMLHttpRequest();
+    var self = this;
+    xhr.onreadystatechange = function () {
+      if (xhr.readyState === 4 && xhr.status === 200) {
+        self.requestSuccess = true;
+      } else {
+        self.requestSuccess = false;
+      }
+      callback.apply(xhr);
+    };
+    xhr.open('GET', this.$.baseUrl.value + '/' + this.$.storeName.value, true);
+    xhr.send();
+  }
+});
