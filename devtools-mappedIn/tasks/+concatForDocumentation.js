974734b391226cfa6bc60a9d2513d81cbd9c80f7
Judit Ross <jtrs@incowia.com>
2016-09-22 08:18:40 +0200

    deleted grunttasks for polymer-analyser + added documentation.html to maniest.webpackage as runnable

--- devtools-mappedIn/tasks/+concatForDocumentation.js
+++
@@ -1,5 +0,0 @@
-module.exports = function (grunt) {
-  'use strict';
-  grunt.registerTask('+concatForDocumentation', 'merge seperate JS Polymer code to its Corrispondent Polymer Element', ['inline']);
-
-};
