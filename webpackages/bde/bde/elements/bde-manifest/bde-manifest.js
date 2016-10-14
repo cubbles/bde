80f854645fc7ac150822f8e46dab74fcd18197d5
Erik Neute <erik.neute@hm-ag.de>
2016-10-14 13:32:04 +0200

    annotate bde-manifest

--- webpackages/bde/bde/elements/bde-manifest/bde-manifest.js
+++ webpackages/bde/bde/elements/bde-manifest/bde-manifest.js
@@ -6,44 +6,95 @@ Polymer({
 
   properties: {
 
+    /**
+     * Name of the Cubble component.
+     *
+     * @type {String}
+     * @property name
+     */
     name: {
       type: String,
       notify: true,
       value: 'new-webpackage'
     },
 
+    /**
+     * Counter of the added component.
+     *
+     * @type {Number}
+     * @property createCounter
+     */
     createCounter: {
       type: Number,
       value: 1
     },
 
+    /**
+     * Group id of the component.
+     * Can be modfied via the app settings.
+     *
+     * @type {String}
+     * @property groupId
+     */
     groupId: {
       type: String,
       notify: true,
       value: ''
     },
 
+    /**
+     * Version of the component.
+     * Can be modfied via the app settings.
+     *
+     * @type {String}
+     * @property version
+     */
     version: {
       type: String,
       value: '1.0.0-SNAPSHOT',
       notify: true
     },
 
+    /**
+     * Model version of the component.
+     * This cannot be modified in the app, it is set to the default value.
+     *
+     * @type {String}
+     * @property modelVersion
+     */
     modelVersion: {
       type: String,
       value: '8.3.1'
     },
 
+    /**
+     * Document type of the component, which is usually the default 'webpackage'.
+     *
+     * @type {String}
+     * @property docType
+     */
     docType: {
       type: String,
       value: 'webpackage'
     },
 
+    /**
+     * Description of the Cubble component.
+     *
+     * @type {String}
+     * @property description
+     */
     description: {
       type: String,
       notify: true
     },
 
+    /**
+     * Author of the Cubble component, object containing name and email address.
+     *
+     * @type {Object}
+     * @property author
+     */
     author: {
       type: Object,
       notify: true,
@@ -55,6 +106,12 @@ Polymer({
       }
     },
 
+    /**
+     * List of contributors to the component.
+     *
+     * @type {Array}
+     * @property contributors
+     */
     contributors: {
       type: Array,
       notify: true,
@@ -63,17 +120,36 @@ Polymer({
       }
     },
 
+    /**
+     * Licence under which the Cubble component is published.
+     * MIT license is the default value, see [https://opensource.org/licenses/MIT]
+     *
+     * @type {String}
+     * @property license
+     */
     license: {
       type: String,
       value: 'MIT',
       notify: true
     },
 
+    /**
+     * Homepage of the author/project, who is publighing this Cubble component.
+     *
+     * @type {String}
+     * @property homepage
+     */
     homepage: {
       type: String,
       notify: true
     },
 
+    /**
+     * Keywords for the Cubble component, is used in the base fopr referencing and searching for components.
+     *
+     * @type {Array}
+     * @property keywords
+     */
     keywords: {
       type: Array,
       value: function () {
@@ -82,6 +158,12 @@ Polymer({
       notify: true
     },
 
+    /**
+     * Manual(s) for the component, represented by an URL to a document.
+     *
+     * @type {Array}
+     * @property man
+     */
     man: {
       type: Array,
       value: function () {
@@ -90,6 +172,12 @@ Polymer({
       notify: true
     },
 
+    /**
+     * Runnables of the Cubble component, e.g. a demo page within the webpackage.
+     *
+     * @type {Array}
+     * @property runnables
+     */
     runnables: {
       type: Array,
       value: function () {
@@ -98,6 +186,12 @@ Polymer({
       notify: true
     },
 
+    /**
+     * Artifacts of the Cubble component. The 'main' metadata of the components for representation in the BDE with slots and connections beeing the main visual input in the app.
+     *
+     * @type {Object}
+     * @property artifacts
+     */
     artifacts: {
       type: Object,
       notify: true,
@@ -145,16 +239,19 @@ Polymer({
   //   'library-update-required': 'onLibraryUpdate'
   // },
 
+  /**
+   * Polymer attached function, sets data from the app.
+   */
   attached: function () {
     document.querySelector('bde-app').set('selectedArtifact', this.artifacts.compoundComponents[ 0 ]);
     document.querySelector('bde-app').set('manifest', this);
   },
 
   /**
-   * Load a new manifest
+   * Load a new manifest, fires event on finish.
    *
-   * @param  {[type]} manifest [description]
-   * @return {[type]}          [description]
+   * @param  {[Object]} manifest [description]
+   * @method loadManifest
    */
   loadManifest: function (manifest) {
     this.fire('bde-manifest-loading');
@@ -171,11 +268,10 @@ Polymer({
   },
 
   /**
-   * Output current manifest in a base-compatible format
+   * Output current manifest in a base-compatible format.
    *
-   * @param  {Function|Array} replacer (optional)
-   * @param  {String|Number}  space    (optional)
-   * @return {String}
+   * @return {Object}
+   * @method toValidManifest
    */
   toValidManifest: function () {
     var manifest = {};
@@ -203,6 +299,12 @@ Polymer({
 
     return manifest;
   },
+
+  /**
+   * Resets the elements data to predefined deafult values.
+   *
+   * @method reset
+   */
   reset: function () {
     var artifactId = this._createArtifactId();
     this.set('name', 'new-webpackage');
@@ -255,12 +357,26 @@ Polymer({
     });
     document.querySelector('bde-app').set('selectedArtifact', this.artifacts.compoundComponents[ 0 ]);
   },
+
+  /**
+   * Create a defined artifactId String
+   *
+   * @return {[String]} [new artifactid]
+   * @method _createArtifactId
+   */
   _createArtifactId: function () {
     var counter = this.get('createCounter');
     var id = 'new-compound-' + counter++;
     this.set('createCounter', counter);
     return id;
   },
+
+  /**
+   * Helper function for filter rules for allowed keys.
+   * @param  {[type]} key [description]
+   * @return {[type]}     [description]
+   * @method _filterRules
+   */
   _filterRules: function (key) {
     var keyAllowed = key !== 'metadata';
     keyAllowed = keyAllowed && key !== 'createCounter';
