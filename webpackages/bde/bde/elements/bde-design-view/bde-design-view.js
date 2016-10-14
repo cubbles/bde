7c25945b361f1f7ec9bc3857c7169994362b29b0
Erik Neute <erik.neute@hm-ag.de>
2016-10-14 14:39:18 +0200

    annotate bde-design-view

--- webpackages/bde/bde/elements/bde-design-view/bde-design-view.js
+++ webpackages/bde/bde/elements/bde-design-view/bde-design-view.js
@@ -1,66 +1,150 @@
+// @importedBy bde-design-view.html
 /*global XMLHttpRequest*/
+
 Polymer({
   is: 'bde-design-view',
+
   properties: {
+    /**
+     * Represents the manifest metadata of the current compound component.
+     *
+     * @type {Object}
+     * @property currentComponentMetadata
+     */
     currentComponentMetadata: {
       type: Object,
       notify: true
     },
+
+    /**
+     * Date Object, set when the component is loaded.
+     *
+     * @type {Object}
+     * @property lastGeneratedTemplateBlobDocTime
+     */
     lastGeneratedTemplateBlobDocTime: {
       type: Object
     },
 
+    /**
+     * The selected page, i.e. dataflowView.
+     *
+     * @type {String}
+     * @property selectedPage
+     */
     selectedPage: {
       notify: true,
       type: String
     },
 
+    /**
+     * Date object for last change.
+     *
+     * @type {Object}
+     * @property lastChangeTime
+     */
     lastChangeTime: {
       type: Object
     },
+
+    /**
+     * The selected compound component.
+     *
+     * @type {Object}
+     * @property selectedCompound
+     */
     selectedCompound: {
       type: Object,
       value: null
     },
+
+    /**
+     * Check value for compatible templates.
+     *
+     * @type {Boolean}
+     * @property compatibleTemplate
+     */
     compatibleTemplate: {
       type: Boolean,
       value: false
     },
+
+    /**
+     * Visibility property, is set to true on open.
+     *
+     * @type {Boolean}
+     * @property isVisible
+     */
     isVisible: {
       type: Boolean,
       value: false
     },
+
+    /**
+     * Disabled designView property is set, when template is incompatible.
+     *
+     * @type {Boolean}
+     * @property designViewDisabled
+     */
     designViewDisabled: {
       type: Boolean,
       value: false
     },
+
+    /**
+     * EndpointId of the current component
+     *
+     * @type {String}
+     * @property lastEndpointId
+     */
     lastEndpointId: {
       type: String
     }
   },
+
   listeners: {
     'confirmEnablingDialog.iron-overlay-closed': 'confirmEnablingDialogCloseHandler',
     'no-compatible-template': 'noCompatibleTemplateHandler',
     'compatible-template': 'compatibleTemplateHandler'
   },
+
   observers: [
     'currentComponentMetadataChanged(currentComponentMetadata.*)'
   ],
 
+  /**
+   * Polymer ready function. Setting event listeners.
+   */
   ready: function () {
     this.parentNode.addEventListener('iron-deselect', this.leaveHandler);
     this.parentNode.addEventListener('iron-select', this.openHandler.bind(this));
   },
 
+  /**
+   * Helper function to set selectedPage on tap to dataflowView.
+   *
+   * @method handleTap
+   */
   handleTap: function () {
     this.selectedPage = "dataflowView";
   },
 
+  /**
+   * Sets properties concordingly, when a template is compatible.
+   *
+   * @method compatibleTemplateHandler
+   *
+   */
   compatibleTemplateHandler: function () {
     this.set('compatibleTemplate', true);
     this.set('designViewDisabled', false);
   },
 
+  /**
+   * Sets properties concordingly, when a template is iscompatible.
+   *
+   * @method noCompatibleTemplateHandler
+   */
   noCompatibleTemplateHandler: function () {
     this.set('compatibleTemplate', false);
     this.set('designViewDisabled', true);
@@ -69,6 +153,11 @@ Polymer({
     }
   },
 
+  /**
+   * Observer for the currentComponentMetadata property. Loads new component data on change.
+   *
+   * @method currentComponentMetadataChanged
+   */
   currentComponentMetadataChanged: function () {
     if (!this.currentComponentMetadata || !this.currentComponentMetadata.manifest || !this.currentComponentMetadata.artifactId || !this.currentComponentMetadata.endpointId) {
       return;
@@ -78,6 +167,11 @@ Polymer({
     }, 2);
   },
 
+  /**
+   * Loads new compound component data.
+   *
+   * @method loadNewCompound
+   */
   loadNewCompound: function () {
     this.set('designViewDisabled', false);
     var compoundComponents = this.currentComponentMetadata.manifest.artifacts.compoundComponents;
@@ -91,6 +185,12 @@ Polymer({
     }.bind(this));
   },
 
+  /**
+   * Helper function for opening the designView.
+   *
+   * @param  {[Event]} e [Tap event]
+   * @method openHandler
+   */
   openHandler: function (e) {
     var designView = e.detail.item;
     if (designView.id !== 'designView') {
@@ -102,6 +202,12 @@ Polymer({
     }
   },
 
+  /**
+   * Helper function for closing designView.
+   *
+   * @param  {[Event]} e [tap event]
+   * @method confirmEnablingDialogCloseHandler
+   */
   confirmEnablingDialogCloseHandler: function (e) {
     if (e.detail.confirmed) {
       this.set('designViewDisabled', false);
@@ -111,8 +217,13 @@ Polymer({
     }
   },
 
+  /**
+   * Helper function for leaving designView, sets the view invisible, and the concording properties to false.
+   *
+   * @method leaveHandler
+   */
   leaveHandler: function (e) {
-    // TODO mapping für blob URL zu webpackage Generierung erstellen und global verwalten
+    // TODO create global mapping for blob url for webpackage generation
     var designView = e.detail.item;
     if (designView.id !== 'designView' || designView.designViewDisabled) {
       return;
@@ -120,6 +231,12 @@ Polymer({
     designView.set('isVisible', false);
   },
 
+  /**
+   * Returns an array of endpoints from the current component.
+   *
+   * @param  {[String]} endpointId [Id of the endpoint of the current component]
+   * @return {[Array]}            [Array of endpoints]
+   */
   getEndpointFromSelectedCompound: function (endpointId) {
     for (var i = 0; i < this.selectedCompound.endpoints.length; i++) {
       if (this.selectedCompound.endpoints[i].endpointId === endpointId) {
@@ -129,6 +246,11 @@ Polymer({
     return;
   },
 
+  /**
+   * Get HTML resources out of the webpackage url.
+   *
+   * @method loadHtmlResources
+   */
   loadHtmlResources: function () {
     if (!this.currentComponentMetadata || !this.currentComponentMetadata.endpointId) {
       return;
@@ -158,6 +280,13 @@ Polymer({
     }
   },
 
+  /**
+   * Create and XMLHttpRequest to load the resources from the webpackage url.
+   *
+   * @param  {[String]} url             [webpackage url]
+   * @param  {[type]} successCallback [description]
+   * @method loadFile
+   */
   loadFile: function (url, successCallback) {
     var xhr = new XMLHttpRequest();
     xhr.onreadystatechange = function () {
@@ -173,6 +302,13 @@ Polymer({
     xhr.send();
   },
 
+  /**
+   * Create the url for the webpackage out of the webpackage metadata.
+   *
+   * @param  {[type]} resourceUrl [description]
+   * @return {[String]}             [url sting]
+   * @method buildSelectedCompoundResourceUrl
+   */
   buildSelectedCompoundResourceUrl: function (resourceUrl) {
     var webpackageQName = '';
     webpackageQName = this.currentComponentMetadata.manifest.name + '@' + this.currentComponentMetadata.manifest.version;
@@ -184,6 +320,14 @@ Polymer({
     return this.relativeToAbsolute(baseUrl, resourceUrl);
   },
 
+  /**
+   * Helper function to create absolute url.
+   *
+   * @param  {[type]} baseUrl     [description]
+   * @param  {[type]} relativeUrl [description]
+   * @return {[type]}             [description]
+   * @method relativeToAbsolute
+   */
   relativeToAbsolute: function (baseUrl, relativeUrl) {
     var stack = baseUrl.split('/');
     var parts = relativeUrl.split('/');
